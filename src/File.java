9e6526f9fcd0f514c85091eb031a5cbe05924d73
NaveenN55 <43067210+NaveenN55@users.noreply.github.com>
2018-10-01 08:32:41 -0400

    Add files via upload

---
+++ src/File.java
@@ -0,0 +1,20 @@
+
+public class File {
+
+	public static void main(String[] args) 
+	{
+		int number= 20;
+		number = number + 5;
+		System.out.println("Add 5:"+number);
+		number/= 2;
+		System.out.println("Divide 2:"+number);
+		number*=4;
+		System.out.println("Multiply 4:"+number);
+		number-=6;
+		System.out.println("Subtract 6:"+number);
+		number%=2;
+		System.out.println("Modularly divide by 2:"+number);
+		
+	}
+
+}
